7c8c5ac9baa39dac04010eb0328a8a3de22d4760
sourav arya <souravarya4u@gmail.com>
2019-08-29 19:48:05 +0530

    added toggle in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,46 +22,74 @@ const Section = styled.section`
   }
 `;
 
-const Header = () => (
-  <Section className="section">
-    <div className="container">
-      <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <Link className="navbar-item" to="/">
-            <img src="/images/packrs-logo.png" alt="Packrs logo" />
-          </Link>
-          <a
-            role="button"
-            className="navbar-burger burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isActive: false,
+    };
+  }
+
+  MobileMenu() {
+    const { isActive } = this.state;
+
+    this.setState({
+      isActive: !isActive,
+    });
+  }
+
+  render() {
+    const { isActive } = this.state;
+    return (
+      <Section className="section">
+        <div className="container">
+          <nav
+            className="navbar"
+            role="navigation"
+            aria-label="main navigation"
           >
-            <span aria-hidden="true" />
-            <span aria-hidden="true" />
-            <span aria-hidden="true" />
-          </a>
-        </div>
-        <div className="navbar-menu">
-          <div className="navbar-start">
-            <Link className="navbar-item">Home</Link>
-            <Link className="navbar-item">About</Link>
-            <Link className="navbar-item">Support</Link>
-          </div>
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <div className="buttons">
-                <Link className="button is-primary">
-                  <strong>Become a Rider</strong>
-                </Link>
-                <Link className="button is-light">Become a Partner</Link>
+            <div className="navbar-brand">
+              <Link className="navbar-item" to="/">
+                <img src="/images/packrs-logo.png" alt="Packrs logo" />
+              </Link>
+              <a
+                role="button"
+                className={
+                  isActive
+                    ? 'navbar-burger burger mobile is-active'
+                    : 'navbar-burger burger mobile'
+                }
+                aria-label="menu"
+                aria-expanded="false"
+                data-target="navbarBasicExample"
+                onClick={() => this.MobileMenu()}
+              >
+                <span aria-hidden="true" />
+                <span aria-hidden="true" />
+                <span aria-hidden="true" />
+              </a>
+            </div>
+            <div className={isActive ? 'navbar-menu is-active' : 'navbar-menu'}>
+              <div className="navbar-start">
+                <Link className="navbar-item">Home</Link>
+                <Link className="navbar-item">About</Link>
+                <Link className="navbar-item">Support</Link>
+              </div>
+              <div className="navbar-end">
+                <div className="navbar-item">
+                  <div className="buttons is-hidden-mobile">
+                    <Link className="button is-primary">
+                      <strong>Become a Rider</strong>
+                    </Link>
+                    <Link className="button is-light">Become a Partner</Link>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+          </nav>
         </div>
-      </nav>
-    </div>
-  </Section>
-);
-
-export default Header;
+      </Section>
+    );
+  }
+}
