133282782f024f298a388aa1f8bc74d15ac85ab1
sourav arya <souravarya4u@gmail.com>
2019-08-29 20:26:28 +0530

    added social link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'gatsby';
 
 const Section = styled.section`
   background-color: ${props => props.theme.darkShades};
