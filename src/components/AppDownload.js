792625bd47f6c5a2665b2fc1c26862829b9b6604
Parminder Klair <perminder.klair@gmail.com>
2019-08-30 22:59:55 +0530

    added app download mockup

--- src/components/AppDownload.js
+++ src/components/AppDownload.js
@@ -1,18 +1,61 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import config from '../utils/config';
+
 const Section = styled.section`
-  margin-top: 4rem;
-  margin-bottom: 4rem;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  img.mockup {
+    height: 400px;
+    width: auto;
+    transform: rotate(20deg);
+  }
+  .title {
+    margin-top: 5rem;
+  }
+`;
+
+const StoreContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+  @media screen and (max-width: 600px) {
+    display: flex;
+  }
+  img {
+    margin-right: 10px;
+    width: auto;
+    height: 42px;
+  }
 `;
 
 const AppDownload = () => (
   <Section className="section">
-    <div className="container">
-      <h2 className="title is-2 has-text-centered has-text-weight-bold">
-        Subscribe
-      </h2>
-    </div>
+    <h2 className="container">
+      <div className="columns is-centered">
+        <div className="column is-two-fifths">
+          <h3 className="title is-3 has-text-centered has-text-weight-bold">
+            Download the Packrs app
+          </h3>
+          <StoreContainer>
+            <a href={config.iosStore}>
+              <img src="/images/store-ios.png" alt="Packrs ios store" />
+            </a>
+            <a href={config.androidStore}>
+              <img src="/images/store-android.png" alt="Packrs android store" />
+            </a>
+          </StoreContainer>
+        </div>
+        <div className="column is-two-fifths">
+          <img
+            className="mockup"
+            src="/images/mockup-dashboard.png"
+            alt="packrs dashboard mockup"
+          />
+        </div>
+      </div>
+    </h2>
   </Section>
 );
 
