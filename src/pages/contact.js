d25c5f37da45b96f7da02980f2c8ff4cfb21a0a2
sourav arya <souravarya4u@gmail.com>
2019-08-30 21:25:52 +0530

    minor fixes in contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,6 +5,9 @@ import styled from 'styled-components';
 import Layout from '../components/Layout';
 
 const Section = styled.div`
+  .container {
+    margin-top: 4rem;
+  }
   p {
     margin-bottom: 1rem;
   }
@@ -27,41 +30,46 @@ const Contact = () => (
       <div className="container">
         <div className="columns">
           <div className="column">
-            <h2 className="title is-1 has-text-weight-bold">Contact Us</h2>
-            <p>
-              We’re as accessible as your good neighbour. So much so that you
-              <br />
-              can hand us over your keys too
-            </p>
-            <p className="is-4">
-              <span role="img" aria-label="e-mail">
-                📧
-              </span>{' '}
-              <a href="mailto:hi@packrs.co">hi@packrs.co</a>
-            </p>
-            <p>
-              <span role="img" aria-label="Round Pushpin">
-                📍
-              </span>{' '}
-              Punjab, India
-            </p>
-            <p>
-              <span role="img" aria-label="telephone">
-                ☎️
-              </span>{' '}
-              <a href="tel:+911815195423">+91-181-5195423</a>
-            </p>
-            <p className="control">
-              <a
-                href="https://packrs.freshdesk.com/support/tickets/new"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <button type="submit" className="button is-secondary is-medium">
-                  get in touch
-                </button>
-              </a>
-            </p>
+            <div className="media-content">
+              <h2 className="title is-1 has-text-weight-bold">Contact Us</h2>
+              <p>
+                We’re as accessible as your good neighbour. So much so that you
+                <br />
+                can hand us over your keys too
+              </p>
+              <p className="is-4">
+                <span role="img" aria-label="e-mail">
+                  📧
+                </span>{' '}
+                <a href="mailto:hi@packrs.co">hi@packrs.co</a>
+              </p>
+              <p>
+                <span role="img" aria-label="Round Pushpin">
+                  📍
+                </span>{' '}
+                Punjab, India
+              </p>
+              <p>
+                <span role="img" aria-label="telephone">
+                  ☎️
+                </span>{' '}
+                <a href="tel:+911815195423">+91-181-5195423</a>
+              </p>
+              <p className="control">
+                <a
+                  href="https://packrs.freshdesk.com/support/tickets/new"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button
+                    type="submit"
+                    className="button is-secondary is-medium"
+                  >
+                    get in touch
+                  </button>
+                </a>
+              </p>
+            </div>
           </div>
           <div className="column">
             <img className="image" src="/images/contact.jpg" alt="box" />
